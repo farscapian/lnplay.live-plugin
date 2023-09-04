327d2fbf30ebf5030cd39e34da0ed47ba28f9aae
Derek Smith <derek@farscapian.com>
2023-09-04 09:26:46 -0400

    Change label to id.

--- README.md
+++ README.md
@@ -4,34 +4,35 @@ Core Lighting plugin for lnplay.live. Developed for the tabconf2023 hackathon.
 
 ## lnplaylive-createorder
 
-`lnplaylive-createorder` is called by the front end web app to get an invoice for a product. Required parameters are node count (see product definition), number of hours the environment should be active (`hours`). The `description` field is any VALID JSON string that the front end wishes to include in the actual BOLT11 invoice. The front end and backend can agree on a format for this, but generally speaking, product customizations other details can be stored in this field. Note: the backend MAY embed additional details (e.g., version/git commits) in the `description` field. Invoice are created with a 300 second timeout (5 minutes).
-
+`lnplaylive-createorder` is called by the front end web app to get an invoice for a product. Required parameters are node count (see product definition), number of hours the environment should be active (`hours`) (minimum 3). The `description` field is any VALID JSON string that the front end wishes to include in the actual BOLT11 invoice. The front end and backend can agree on a format for this, but generally speaking, product customizations other details can be stored in this field. Note: the backend MAY embed additional details (e.g., version/git commits) in the `description` field. Invoice are created with a 300 second timeout (5 minutes).
 
 ### example
 
 ```bash
-lightning-cli -k lnplaylive-createorder node_count=16 hours=48 description="test"
+lightning-cli-k lnplaylive-createorder node_count=16 hours=3 description=" {\"field0\": true, \"field1\": testing}"
 {
    "node_count": 16,
-   "hours": 48,
-   "bolt11_invoice_label": "9ebc0af9-fe5e-4b17-8683-fd689e486743",
-   "bolt11": "lnbcrt800n1pj0tjlrsp5u...786gqalz9tg"
+   "hours": 3,
+   "bolt11_invoice_id": "aec62c5a-3dcb-41d6-ab85-3b8da6b5356f",
+   "bolt11_invoice": "lnbcrt3520n1pj0tkwksp5...9h53z",
+   "bolt12_invoice_id": "fc1946bb-1ca2-41f8-b352-ddf7d919c601",
+   "bolt12_invoice": "TODO"
 }
 ```
 
 ## lnplaylive-invoicestatus
 
-The `lnplaylive-invoicestatus` rpc method returns the status of a BOLT11 invoice. The two required parameters are `payment_type`, which can be either 'bolt11' or 'bolt12', and `invoice_label`, which is [invoice label](https://docs.corelightning.org/reference/lightning-invoice) in the CLN database and is provided in the `lnplaylive-createorder` return value.
+The `lnplaylive-invoicestatus` rpc method returns the status of a BOLT11 invoice. The two required parameters are `payment_type`, which can be either 'bolt11' or 'bolt12', and `invoice_id`, which is [invoice label](https://docs.corelightning.org/reference/lightning-invoice) in the CLN database and is provided in the `lnplaylive-createorder` return value.
 
 The front end can poll this rpc command to see if there are updates to invoice. The invoice status can change to `paid` or `expired`.
 
 ### example
 
 ```bash
-lightning-cli -k lnplaylive-invoicestatus payment_type=bolt11 invoice_label="9ebc0af9-fe5e-4b17-8683-fd689e486743"
+lightning-cli -k lnplaylive-invoicestatus payment_type=bolt11 invoice_id="aec62c5a-3dcb-41d6-ab85-3b8da6b5356f"
 {
    "payment_type": "bolt11",
-   "invoice_label": "9ebc0af9-fe5e-4b17-8683-fd689e486743",
+   "invoice_id": "aec62c5a-3dcb-41d6-ab85-3b8da6b5356f",
    "invoice_status": "unpaid"
 }
 ```
--- lnplay-live.py
+++ lnplay-live.py
@@ -30,27 +30,49 @@ def init(options, configuration, plugin, **kwargs):
 def lnplaylive_createorder(plugin, node_count, hours, description):
     '''Returns a BOLT11 invoice for the given node count and time.'''
     try:
+        # first let's ensure the values that they passed in are appropriate.
+        rate_to_charge = None
+
+        if node_count == 8:
+            rate_to_charge = 20000
+        elif node_count == 16:
+            rate_to_charge = 22000
+        elif node_count == 32:
+            rate_to_charge = 24000
+        elif node_count == 64:
+            rate_to_charge = 26000
+        else:
+            raise InvalidCLNCountError("Invalid product. Valid node counts are 8, 16, 32, and 64.")
+
+        if not isinstance(hours, int):
+            raise Exception("Error: hours MUST be a positive integer.")
+
+        if hours < 3:
+            raise HoursTooLowException("The minimum hours you can set is 3.")
+        elif hours > 504:
+            raise HoursTooHighException("The maximum hours you can set is 504.")
+
+        # calcuate the amount to charge in the invoice (msats)
+        amount_to_charge = rate_to_charge * node_count
+
         ## we just need a guid to for cross referencing invoices.
         bolt11_guid = uuid.uuid4()
         bolt11_guid_str = str(bolt11_guid)
         
-        # rate is determined by product; in msats.
-        rate_to_charge = 5000
-        amount_to_charge = rate_to_charge * node_count
+        # generate the invoice
         bolt11_invoice = plugin.rpc.invoice(amount_to_charge, bolt11_guid_str, description, 300)
 
+        # create a BOLT12 offer.
         bolt12_label = str(uuid.uuid4())
-        bolt12_invoice = "TODO"
         bolt12_invoice = "TODO"
 
         createorder_response = {
             "node_count": node_count,
             "hours": hours,
-            "bolt11_invoice_label": bolt11_guid_str,
-            "bolt11": bolt11_invoice["bolt11"]
-            #,
-            #"bolt12_invoice_label": bolt12_label,
-            #"bolt12_invoice": bolt12_invoice
+            "bolt11_invoice_id": bolt11_guid_str,
+            "bolt11_invoice": bolt11_invoice["bolt11"],
+            "bolt12_invoice_id": bolt12_label,
+            "bolt12_invoice": bolt12_invoice
         }
 
         json_data = json.dumps(createorder_response)
@@ -63,7 +85,7 @@ def lnplaylive_createorder(plugin, node_count, hours, description):
 
 # todo over time convert this to the wait rpc semantics.
 @plugin.method("lnplaylive-invoicestatus")
-def lnplaylive_invoicestatus(plugin, payment_type, invoice_label):
+def lnplaylive_invoicestatus(plugin, payment_type, invoice_id):
     '''Retuns the status of an invoice.'''
 
     try:
@@ -73,17 +95,20 @@ def lnplaylive_invoicestatus(plugin, payment_type, invoice_label):
             raise InvalidEnumerationError("Invalid payment type. Should be 'bolt11' or 'bolt12'.")
 
         # get info about the invoice and return it to the caller.
-        invoices = plugin.rpc.listinvoices(invoice_label)
+        invoices = plugin.rpc.listinvoices(invoice_id)
 
         matching_invoice = None
         for invoice in invoices["invoices"]:
-            if invoice.get("label") == invoice_label:
+            if invoice.get("label") == invoice_id:
               matching_invoice = invoice
               break
 
+        if matching_invoice is None:
+            raise InvoiceNotFoundError("BOLT11 invoice not found. Wrong invoice_id?")
+
         invoicestatus_response = {
             "payment_type": payment_type,
-            "invoice_label": invoice_label,
+            "invoice_id": invoice_id,
             "invoice_status": matching_invoice["status"]
         }
 
@@ -99,10 +124,27 @@ def lnplaylive_invoicestatus(plugin, payment_type, invoice_label):
 class InvalidEnumerationError(Exception):
     pass
 
+class InvoiceNotFoundError(Exception):
+    pass
+
+class InvalidCLNCountError(Exception):
+    pass
+
+class HoursTooLowException(Exception):
+    pass
+
+class HoursTooHighException(Exception):
+    pass
+
+class WhatTheHellException(Exception):
+    pass
+    
+
 @plugin.subscribe("invoice_payment")
 def on_payment(plugin, invoice_payment, **kwargs):
     try:
-        return "{}"
+        value = "test"
+
     except RpcError as e:
         printout("Payment error: {}".format(e))
 
