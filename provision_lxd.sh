8f927816d798c7d60b8fd6bd10734c84e7088358
Derek Smith <derek@farscapian.com>
2023-09-07 15:03:41 -0400

    Updates

--- provision_lxd.sh
+++ provision_lxd.sh
@@ -2,4 +2,26 @@
 
 set -exu
 
-lxc remote list
+if ! lxc remote list | grep -q lnplaylive; then
+    lxc remote add lnplaylive "$LNPLAY_LXD_FQDN_PORT" --password "$LNPLAY_LXD_PASSWORD" --accept-certificate >> /dev/null
+# else
+#     echo "INFO: remote 'lnplaylive' already exists."
+fi
+
+if ! lxc remote get-default | grep -q lnplaylive; then
+    lxc remote switch lnplaylive  >> /dev/null
+# else
+#     echo "INFO: lxc remote already on remote lnplaylive"
+fi
+
+if ! lxc project list | grep -q lnplaylivemvp; then
+    lxc project create lnplaylivemvp
+fi
+
+if ! lxc project list | grep -q "default (current)"; then
+    lxc project switch lnplaylivemvp
+fi
+
+cd /sovereign-stack
+bash -c "$(pwd)/deployment/up.sh"
+
